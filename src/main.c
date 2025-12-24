e472b7e7c001d5b5c766efd9eb4da4eb2ceb99cb
7sev7nsev7n7 <7sev7nsev7n7@gmail.com>
2025-12-24 00:15:32 -0600

    Added exit upon no arguments passed, as this was accidentally ommited

--- src/main.c
+++ src/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <b64/cdecode.h>
 #include "structs.h"
 
 int main(int argc, char *argv[]) {
   // argument checking
   if (argc<2) {
     printf("no files specified\n");
+    exit(1);
   } 
   FILE *fptr = fopen(argv[1], "r");
   if (!fptr) {
